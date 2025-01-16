9240b5aeb897e0ae15f22b36e30827f0457c372b
“uday” <udaycloud.training@gmail.com>
2025-01-16 11:29:20 +0530

    updated with my custom scripts,k8sManifests and azure devops pipelines

---
+++ scripts/Myupdatek8smanifests.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -x
+
+# Set the repository URL
+REPO_URL="https://8TNFWqbWxOOKcxWp7mlyhPV1Ew14SBVqwFpt5xJWAEp7gWw2bIblJQQJ99BAACAAAAAAAAAAAAASAZDOtO2E@dev.azure.com/udaycloudtraining/Voting-app/_git/Voting-app"
+
+# Clone the git repository into the /tmp directory
+git clone "$REPO_URL" /tmp/temp_repo
+
+# Navigate into the cloned repository directory
+cd /tmp/temp_repo
+
+# Make changes to the Kubernetes manifest file(s)
+# For example, let's say you want to change the image tag in a deployment.yaml file
+sed -i "s|image:.*|image: azcicr.azurecr.io/$2:$3|g" k8s-specifications/$1-deployment.yaml
+
+# Add the modified files
+git add .
+
+# Commit the changes
+git commit -m "Update Kubernetes manifest"
+
+# Push the changes back to the repository
+git push
+
+# Cleanup: remove the temporary directory
+rm -rf /tmp/temp_repo
